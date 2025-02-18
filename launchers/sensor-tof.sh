bcb30d048a6bed14efd896579ad031f0274aa7ee
Liam Paull <paulll@iro.umontreal.ca>
2025-02-17 21:26:51 -0500

    fixing TOF sensor no longer being run on duckiebot (but constantly restarting the container)

--- launchers/sensor-tof.sh
+++ launchers/sensor-tof.sh
@@ -10,10 +10,11 @@ source /environment.sh
 
 # NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
-
 # If the environment variable ROBOT_CONFIGURATION is set to DD24, run dt-launcher-sensor-tof-bottom
 if [ "$ROBOT_CONFIGURATION" == "DD24" ]; then
     dt-launcher-sensor-tof-bottom
+elif [ "$ROBOT_CONFIGURATION" == "DB21J" ]; then
+    dt-launcher-sensor-tof-front-center
 fi
 
 
