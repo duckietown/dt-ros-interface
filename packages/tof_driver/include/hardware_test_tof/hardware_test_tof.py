2d7a9ac5a6bdb178ce858b31011f82006b024895
Liam McAlpine <liam.mcalpine@duckietown.com>
2025-08-18 13:43:09 +0200

    Refactor HardwareTestToF to extend AbstractHardwareTestROSInterface and integrate into ToFNode for improved hardware testing functionality

--- packages/tof_driver/include/hardware_test_tof/hardware_test_tof.py
+++ packages/tof_driver/include/hardware_test_tof/hardware_test_tof.py
@@ -1,44 +1,39 @@
-import rospy
+from typing import Any
 
-from dt_duckiebot_hardware_tests import HardwareTest, HardwareTestJsonParamType
-
-from tof_accuracy import ToFAccuracy
+from duckiebot_hardware_test_ros_interface import AbstractHardwareTestROSInterface
 
 
-class HardwareTestToF(HardwareTest):
-    def __init__(self, sensor_name: str, accuracy: ToFAccuracy) -> None:
-        super().__init__()
-        # attr
-        self._sensor_name = sensor_name  # e.g. "front_center"
-        self._sensor_name_human = sensor_name.replace("_", " ")  # e.g. "front center"
-        self._accuracy = accuracy
+class HardwareTestToF(AbstractHardwareTestROSInterface):
+    sensor_name: str
 
-    def test_id(self) -> str:
-        return f"Time-of-Flight ({self._sensor_name})"
+    def __init__(self, node: Any, sensor_name: str) -> None:
+        self.sensor_name = sensor_name
+        super().__init__(node, f"Time-of-Flight ({self.sensor_name})")
+
+    def cb_run_test(self, _):
+        return self.format_response_stream(
+            success=True,  # does not matter here
+            test_topic_name=f"{self.sensor_name}_tof_driver_node/range",
+            test_topic_type="sensor_msgs/Range",
+            lst_blocks=[],
+        )
+
+    def get_test_data(self, _: dict) -> dict:
+        return {}
+
+    def test_description_expectation(self) -> str:
+        sensor_name_human = self.sensor_name.replace("_", " ")
+        return self.html_util_ul(
+            [
+                "Once your start the test, a <strong>Range:</strong> field will appear below.",
+                f"When you move your hand closer and farther to the <strong>{sensor_name_human}</strong> ToF, the range reading should change accordingly, i.e. moving closer leads to a smaller value, and farther with a greater value.",
+                "The data below should show (in red color) <em>Out of range</em>.",
+            ]
+        )
 
     def test_description_preparation(self) -> str:
         return self.html_util_ul(
             [
                 "Place your Duckiebot on a flat surface with at least one meter of empty space in front of the Time-of-Flight sensor on the front bumper.",
             ]
-        )
-
-    def test_description_expectation(self) -> str:
-        return self.html_util_ul(
-            [
-                "Once your start the test, a <strong>Range:</strong> field will appear below.",
-                f"When you move your hand closer and farther to the <strong>{self._sensor_name_human}</strong> ToF, the range reading should change accordingly, i.e. moving closer leads to a smaller value, and farther with a greater value.",
-                f"The effective range of the sensor is <strong>from {self._accuracy.min_range}m to {self._accuracy.max_range}m</strong>. The data below should show (in red color) <em>Out of range</em>.",
-            ]
-        )
-
-    def cb_run_test(self, _):
-        rospy.loginfo(f"[{self.test_id()}] Test service called.")
-
-        # Return the service response
-        return self.format_response_stream(
-            success=True,  # does not matter here
-            test_topic_name=f"{self._sensor_name}_tof_driver_node/range",
-            test_topic_type="sensor_msgs/Range",
-            lst_blocks=[],
         )
